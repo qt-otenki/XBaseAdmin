f41969005d9d9cc808148a4a991f875f10d84cc3
繁华 <18383219470@163.com>
2018-05-17 13:31:39 +0800

    优化前端校验框架

--- style/js/validation.js
+++ style/js/validation.js
@@ -2,6 +2,12 @@
  * 标签有
  * required  是否校验
  * 
+ * 
+ * 
+ * 设置校验信息回调方式
+ * 1.alert反馈
+ * 2.自定义函数反馈
+ * 3.dialog反馈
  */
 
 
@@ -14,12 +20,38 @@ function checkTag(obj,tagName){
 	return (typeof(obj.attr(tagName)) != "undefined");
 }
 
+/**
+ * 获取某个控件的标签值
+ * @param {Object} obj
+ * @param {Object} tagName
+ */
+function getTagValue(obj,tagName){
+	return obj.attr(tagName);
+}
+
+/**
+ * 获取某个控件的输入值
+ * @param {Object} obj
+ */
+function getInputValue(obj){
+	return obj.val();
+}
+
 function validation(formId){
 	formId = "#" + formId;
 	var validObj = $(formId);
+	var status = true;
 	$(formId +" input").each(function(){
-		index = $(this).index();
-		$(this).css({"border-color":"#FF0000"})
+		if(!checkTag($(this),"isPass")){
+			var inputType = getTagValue($(this),"type");
+			var inputValue = getInputValue($(this));
+			if(inputValue.length <= 0){
+				$(this).focus();
+				status = false;
+				return false;
+			}
+			
+		}
 	});
-	return false;
+	return status;
 }
