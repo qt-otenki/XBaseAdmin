a4a0d8cb5535b24a96b6e071226f17c577ff8d7d
繁华 <18383219470@163.com>
2018-10-21 21:00:11 +0800

    找回js文件

--- style/js/admin.js
+++ style/js/admin.js
@@ -13,20 +13,22 @@ var iframe;
 var iframe_refresh;
 var desktop_link;
 
-function initWindow(){
+function initWindow() {
 	var height = $(window).height();
 	var width = $(window).width();
-	
-	$("#wrap").css("height",height - 50);
-	$("#right").css("width",width - 230);
-	$("#iframe").css({"height":height - 110});
+
+	$("#wrap").css("height", height - 50);
+	$("#right").css("width", width - 230);
+	$("#iframe").css({
+		"height": height - 110
+	});
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 	initWindow();
-	
+
 	$("#header_name").text(projectName);
-	$("#right").append("<div id='copyright'>"+ copyRight +"&nbsp;&nbsp;版本号："+ version +"</div>");
+	$("#right").append("<div id='copyright'>" + copyRight + "&nbsp;&nbsp;版本号：" + version + "</div>");
 	document.title = projectName;
 
 	title = $('.childTitle');
@@ -39,82 +41,77 @@ $(document).ready(function(){
 	desktop_link = iframe.attr("src");
 	header = $("#header");
 
-	header.css("background",theme[themeId]);
+	header.css("background", theme[themeId]);
 
-	$("#header_user").mouseover(function(){
+	$("#header_user").mouseover(function() {
 		$("#header_menu").show();
 	});
 
-	$("#header_user").mouseout(function(){
+	$("#header_user").mouseout(function() {
 		$("#header_menu").hide();
 	});
 
-	$("#header_menu a").click(function(){
+	$("#header_menu a").click(function() {
 		var index = $(this).index();
 		var url = $(this).attr("data-href");
-        menuBarOnClick(index,url);
+		menuBarOnClick(index, url);
 	});
 
-
-	iframe_refresh.click(function(){
+	iframe_refresh.click(function() {
 		refreshWindow();
 	});
 
-
 	//这里单击事件需要改变
-	title.click(function(){
+	title.click(function() {
 		var t = $(this).find("span").eq(1).text();
 		var menuItem = $(this).next();
 		var arrow = $(this).children('span').eq(2);
-		
-		if(menuItem.css("display") == "none"){
+
+		if(menuItem.css("display") == "none") {
 			menuItem.slideDown();
 			arrow.removeClass("arrow").addClass("arrow-up");
-		}else{
+		} else {
 			menuItem.slideUp();
 			arrow.removeClass("arrow-up").addClass("arrow");
 		}
 		nav_first.text(t);
 	});
 
-	con.click(function(){
-		return false;  // 阻止事件冒泡
+	con.click(function() {
+		return false; // 阻止事件冒泡
 	});
 
-	link.click(function(){
+	link.click(function() {
 		var linkText = $(this).text();
 		var linkSrc = $(this).attr("data-url");
-		nav_second.attr("src",linkSrc).text(linkText);
+		nav_second.attr("src", linkSrc).text(linkText);
 		openWindow(linkSrc);
 	});
 
 	$(window).resize(function() {
-	  initWindow();
+		initWindow();
 	});
 });
 
 //=======需要继承的方法========
-function menuBarOnClick(index,url){
+function menuBarOnClick(index, url) {
 	alert("请在您的主界面中实现menuBarOnClick(index,url)方法");
 }
 
 //========菜单相关的操作=========
-function openWindow(url){
-	iframe.attr("src",url);
+function openWindow(url) {
+	iframe.attr("src", url);
 }
 
-function refreshWindow(){
+function refreshWindow() {
 	var src = iframe.attr("src");
 	openWindow(src);
 }
 
-function desktop(){
+function desktop() {
 	openWindow(desktop_link);
 }
 
-function getChild(){
+function getChild() {
 	return window.frames['iframe'];
 }
-
-
-
